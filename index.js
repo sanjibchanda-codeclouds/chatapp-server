7b45bb410bb42428465b49f190e3cd36faab32f1
sanjibchanda-codeclouds <sanjib.chanda@codeclouds.in>
2023-12-31 16:21:41 +0530

    index file changes

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chatapp-server-lime.vercel.app/",
     credentials: true,
   },
 });
