8a55e092dded1d9bd4ef73183b4424d4864f796f
sanjibchanda-codeclouds <sanjib.chanda@codeclouds.in>
2024-01-01 16:19:52 +0530

    index changes

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "https://chatapp-react-seven.vercel.app",
+    origin: ["https://chatapp-react-seven.vercel.app"],
     credentials: true,
   },
 });
